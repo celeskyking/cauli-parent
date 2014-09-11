25e440dac8de11456ab953f585fa732674ada8f5
王天庆 <tianqing.wang@qunar.com>
2014-09-11 22:14:02 +0800

    去掉了没用的类

--- cauli-easyserver/src/main/java/org/cauli/mock/TemplateSource.java
+++
@@ -1,7 +0,0 @@
-package org.cauli.mock;
-
-/**
- * Created by tianqing.wang on 2014/9/5
- */
-public class TemplateSource {
-}
--- cauli-easyserver/src/main/java/org/cauli/mock/monitor/ActionMonitor.java
+++
@@ -1,7 +0,0 @@
-package org.cauli.mock.monitor;
-
-/**
- * Created by tianqing.wang on 2014/8/14
- */
-public class ActionMonitor {
-}
