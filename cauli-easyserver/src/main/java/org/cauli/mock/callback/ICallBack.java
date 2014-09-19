980d29a12db09bb9f6554453ba3bea5c9d41d969
王天庆 <tianqing.wang@qunar.com>
2014-09-19 23:08:09 +0800

    fix bugs

---
+++ cauli-easyserver/src/main/java/org/cauli/mock/callback/ICallBack.java
@@ -0,0 +1,16 @@
+package org.cauli.mock.callback;
+
+
+
+/**
+ * Created by tianqing.wang on 2014/9/19
+ */
+public interface ICallback {
+
+    public String build();
+
+    public Response send();
+
+
+
+}
